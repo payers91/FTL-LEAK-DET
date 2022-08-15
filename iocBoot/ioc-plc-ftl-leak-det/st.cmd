494476f127e4ac4ba5af0544bf2ab163f24d1544
Patrick Truesdall Ayers <payers@psbuild-rhel7-01.slac.stanford.edu>
2022-08-15 12:34:56 -0700

    ioc built and screen created and tested

--- iocBoot/ioc-plc-ftl-leak-det/st.cmd
+++ iocBoot/ioc-plc-ftl-leak-det/st.cmd
@@ -1,4 +1,4 @@
-#!c:/Repos/ads-ioc/R0.5.0///bin/rhel7-x86_64/adsIoc
+#!/reg/g/pcds/epics/ioc/common/ads-ioc/R0.5.0/bin/rhel7-x86_64/adsIoc
 ###### AUTO-GENERATED DO NOT EDIT ##############
 
 < envPaths
@@ -6,7 +6,7 @@
 epicsEnvSet("ADS_IOC_TOP", "$(TOP)" )
 
 epicsEnvSet("ENGINEER", "payers" )
-epicsEnvSet("LOCATION", "PLC:plc_ftl_leak_det" )
+epicsEnvSet("LOCATION", "PLC:FTL:LEAK:DET:01" )
 epicsEnvSet("IOCSH_PS1", "$(IOC)> " )
 epicsEnvSet("ACF_FILE", "$(ADS_IOC_TOP)/iocBoot/templates/unrestricted.acf")
 
@@ -55,13 +55,13 @@ adsAsynPortDriverConfigure("$(ASYN_PORT)", "$(IPADDR)", "$(AMSID)", "$(AMS_PORT)
 cd "$(ADS_IOC_TOP)/db"
 
 
-dbLoadRecords("iocSoft.db", "IOC=PLC:plc_ftl_leak_det")
-dbLoadRecords("save_restoreStatus.db", "P=PLC:plc_ftl_leak_det:")
+dbLoadRecords("iocSoft.db", "IOC=PLC:FTL:LEAK:DET:01")
+dbLoadRecords("save_restoreStatus.db", "P=PLC:FTL:LEAK:DET:01:")
 dbLoadRecords("caPutLog.db", "IOC=${IOC}")
 
 ## TwinCat System Databse files ##
-dbLoadRecords("TwinCAT_TaskInfo.db", "PORT=ASYN_PLC, PREFIX=PLC:plc_ftl_leak_det")
-dbLoadRecords("TwinCAT_AppInfo.db", "PORT=ASYN_PLC, PREFIX=PLC:plc_ftl_leak_det")
+dbLoadRecords("TwinCAT_TaskInfo.db", "PORT=ASYN_PLC, PREFIX=PLC:FTL:LEAK:DET:01")
+dbLoadRecords("TwinCAT_AppInfo.db", "PORT=ASYN_PLC, PREFIX=PLC:FTL:LEAK:DET:01")
 
 cd "$(IOC_TOP)"
 
@@ -73,7 +73,7 @@ cd "$(IOC_TOP)"
 set_savefile_path( "$(IOC_DATA)/$(IOC)/autosave" )
 set_requestfile_path( "$(IOC_TOP)/autosave" )
 
-save_restoreSet_status_prefix( "PLC:plc_ftl_leak_det:" )
+save_restoreSet_status_prefix( "PLC:FTL:LEAK:DET:01:" )
 save_restoreSet_IncompleteSetsOk( 1 )
 save_restoreSet_DatedBackupFiles( 1 )
 set_pass0_restoreFile( "info_positions.sav" )
